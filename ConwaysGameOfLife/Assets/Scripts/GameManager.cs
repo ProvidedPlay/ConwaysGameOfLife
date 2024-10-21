7afb22a4a1ac93fd65284b67c410e89750606c8b
ProvidedPlay <mosesreid@hotmail.com>
2024-10-21 01:04:19 -0400

    Created basic conways game. 3 states: game starting (paint tiles using select) game running (follows conways rules) game over (freezes at last frame)
    progress states by pressing enter
    
    needs to be optimized, currently each cell to have adjacent lookups run a lookup on whether or not that adjacent cell exists in all cells twice, once when creating the dict adjacentCells, again when deciding whether to apply logic to that dict

--- ConwaysGameOfLife/Assets/Scripts/GameManager.cs
+++ ConwaysGameOfLife/Assets/Scripts/GameManager.cs
@@ -13,7 +13,13 @@ public class GameManager : MonoBehaviour
     public Color onColor = Color.white;
     public Color offColor = Color.black;
 
-    public List<Vector3Int> allTileLocations; //Tilebase is the base class for a tile, parent to all child tile objects/methods/parameters
+    public int tilemapZAxisPosition;
+
+    public Dictionary<Vector3Int, bool> allTiles = new(); //Dict key = transform, value = isLiving bool
+    public Dictionary<Vector3Int, bool> selectedTiles = new();
+    public Dictionary<Vector3Int, bool> livingCells = new();
+    public Dictionary<Vector3Int, bool> deadCellConsiderationDict = new();
+    public Dictionary<Vector3Int, bool> cellsMarkedForLifeChange = new();
 
     public enum GameState
     {
@@ -23,16 +29,21 @@ public class GameManager : MonoBehaviour
     }
     public GameState gameState;
 
+    [Range(-1, 1)]
+    public float portionOfLivingStartCells;
+
     private void Awake()
     {
         UnpackReferences();
     }
     void Start()
     {
-        SetUpGameBoard();
+        //SetUpGameBoard();
+        GameStart();
     }
     void Update()
     {
+        /*
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             InstantiateTilesPositionArray();
@@ -43,6 +54,11 @@ public class GameManager : MonoBehaviour
                 RandomizeAllColors();
             }
         }
+        if(gameState.ToString() == "GamePlaying")
+        {
+            RunGame();
+        }
+        */
     }
     void UnpackReferences()
     {
@@ -53,38 +69,297 @@ public class GameManager : MonoBehaviour
 
     void SetUpGameBoard()
     {
+        InstantiateTilesPositionArray();
+        /*
         if (gameState.ToString() == "GameSetup")
         {
             InstantiateTilesPositionArray();
         }
+        */
     }
 
     void InstantiateTilesPositionArray()
     {
-        if(tileMapManager != null && tileMap != null && defaultTile != null)
+        if(tileMapManager != null && tileMap != null && defaultTile != null &&allTiles!=null)
         {
-            allTileLocations.Clear();
+            allTiles.Clear();
             foreach(var tilePosition in tileMapManager.tileMapBounds.allPositionsWithin)
             {
-                allTileLocations.Add(tilePosition);
+                //Debug.Log(tilePosition.ToString());
+                allTiles.Add(tilePosition, false);
                 tileMap.SetTile(tilePosition, defaultTile);
                 ColorTile(tilePosition, tileMap, offColor);
             }
-
+            tilemapZAxisPosition = (int)tileMap.transform.position.z;
         }
     }
     void ColorTile(Vector3Int tilePosition, Tilemap tileMap, Color desiredColor)
     {
             tileMap.SetTileFlags(tilePosition, TileFlags.None);
             tileMap.SetColor(tilePosition, desiredColor);
-            print(tilePosition);
     }
 
     void RandomizeAllColors()
     {
-        foreach(var tilePosition in allTileLocations)
+        foreach(var tile in allTiles)
         {
-            ColorTile(tilePosition, tileMap, Random.Range(-1f, 1f) > 0 ? offColor : onColor);
+            //ColorTile(tile.Key, tileMap, Random.Range(-1f, 1f) > portionOfLivingStartCells ? offColor : onColor);
+            KillOrBirthCell(tile.Key, Random.Range(-1f, 1f) < portionOfLivingStartCells);
         }
     }
+    void KillOrBirthCell(Vector3Int tilePosition, bool isCellAlive)
+    {
+        //allTiles[tilePosition] = isCellAlive;
+        ColorTile(tilePosition, tileMap, isCellAlive ? onColor : offColor);
+
+        if (isCellAlive )
+        {
+            livingCells[tilePosition] = true;
+            allTiles[tilePosition] = true;
+        }
+        else if(!isCellAlive)
+        {
+            livingCells.Remove(tilePosition);
+            allTiles[tilePosition ] = false;
+        }
+    }
+    /*
+     * 
+     * COROUTINES
+     *      WRITE COROUTINE FORM:   <accessmodifier> IEnumerator <CoroutineName>()
+     *      CALL COROUTINE FORM:    StartCoroutine(<CoroutineName>())              
+     *              
+     *                                                              //'yield <condition/code>': stops coroutine, waits for 'condition/code' to be met, resumes coroutine
+     */
+
+    private void GameStart()
+    {
+        SetUpGameBoard();
+        StartCoroutine(GameLoop());
+    }
+
+    private IEnumerator GameLoop()
+    {
+        yield return StartCoroutine(GameSetup());
+        yield return StartCoroutine(GamePlaying());
+        yield return StartCoroutine(RoundEnding());
+        GameStart();
+    }
+
+    private IEnumerator GameSetup()
+    {
+        Debug.Log("Coroutine state: game setup");
+        while (!Input.GetKeyDown(KeyCode.Return))
+        {
+            //code in here will run during this game state
+
+            CustomizeGameBoard();
+            yield return null;
+        }
+        //ConsiderLivingCells();//this is a test, remove when actually implementing
+        yield return new WaitForEndOfFrame();
+    }
+    private IEnumerator GamePlaying()
+    {
+        Debug.Log("Coroutine state: game playing");
+        ClearSelectedTiles();
+        while (!Input.GetKeyDown(KeyCode.Return))
+        {
+            //code in here will run during this game state
+            //RandomizeAllColors();
+            /*
+            if (Input.GetKeyDown(KeyCode.LeftControl)){
+                RunCellLifeCycleLoop();
+            }
+            */
+            RunCellLifeCycleLoop();
+            yield return null;
+        }
+        yield return new WaitForEndOfFrame();
+    }
+    private IEnumerator RoundEnding()
+    {
+        Debug.Log("Coroutine state: game ending");
+        while (!Input.GetKeyDown(KeyCode.Return))
+        {
+            //code in here will run during this game state
+
+            yield return null;
+        }
+        yield return new WaitForEndOfFrame();
+    }
+
+    /*
+     * 
+     * Game State Methods
+     * 
+     */
+    void CustomizeGameBoard()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Vector3Int mousePosition = GetTransformAtMousePosition();
+
+            FlipTileAtPosition(mousePosition);
+        }
+        if(Input.GetMouseButtonUp(0))
+        {
+            ClearSelectedTiles();
+        }
+    }
+
+    void FlipTileAtPosition(Vector3Int mousePosition)
+    {
+        //Debug.Log("attempting to flip at "+ mousePosition);
+        if (!selectedTiles.ContainsKey(mousePosition) && allTiles.ContainsKey(mousePosition))
+        {
+            //Debug.Log(mousePosition + " flipped");
+            //allTiles[mousePosition] = !allTiles[mousePosition]; OLD
+            KillOrBirthCell(mousePosition, !allTiles[mousePosition]);
+            AddToSelectedTiles(mousePosition, allTiles[mousePosition]);
+            //Color desiredColor = allTiles[mousePosition] == true ? onColor : offColor; OLD
+            //ColorTile(mousePosition, tileMap, desiredColor);OLD
+        }
+
+    }
+    /*
+     * Tile Selector Methods
+     */
+    Vector3Int GetTransformAtMousePosition()
+    {
+        Vector3Int mousePosition = Vector3Int.FloorToInt(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        mousePosition.z = tilemapZAxisPosition; //this is because the camera z axis is not the same value as the tile z axis, and we want the tile z axis;
+        return mousePosition;
+    }
+    void AddToSelectedTiles(Vector3Int tilePosition, bool tileIsAlive)
+    {
+        selectedTiles.Add(tilePosition, tileIsAlive);
+    }
+    void ClearSelectedTiles()
+    {
+        selectedTiles.Clear();
+    }
+
+    /*
+     * Cell Consideration
+     * create a dict of all living cells (livingCellConsiderationDict)
+     * for each cell in LivingCellConsiderationDict
+     *      for each neighboring cell (consider all cells around it)
+     *          if living: 
+     *              add to livingAdjacentCellCount
+     *          if dead: 
+     *              add to deadCellConsiderationDict
+     *      determineIfCurrentCellSurvives(livingAdjacentCellCount)
+     *          if livingAdjacentCellCount != 2,3
+     *              MarkCellForLifeChange(false) aka kill the cell
+     * clear livingCellConsiderationDict
+     * 
+     * for each cell in deadCellConsiderationDict
+     *      for each neighboring cell (consider all cells around it)
+     *          if living:
+     *              add to livingAdjacentCellCount
+     *      determineIfCurrentCellIsBorn(livingAdjacentCellCount)
+     *          if livingAdjacentCellCount = 3
+     *              MarkCellForLifeChange(true)aka birth the cell
+     *clear deadCellConsiderationDict
+     *              
+     *MarkCellForLifeChange(bool markedForLife, Vector3Int cellPosition)
+     *  cellsMarkedForLifeChange[cellPosition] = markedForLife
+     * 
+     *UpdateCellLifeCycle
+     *  forEach cellMarkedForLifeChange in cellsMarkedForLifeChange
+     *      allTiles[cellMarkedForLifeChange] = cellsMarkedForLifeChange[cellMarkedForLifeChange]
+     *      changeColor(cellMarkedForLifeChange, tilemap, cellsMarkedForLifeChange[cellMarkedForLifeChanged] == true? onColor : offColor
+     *cellsMarkedForLifeChange.Clear()
+     *      
+     *  
+     *              
+     *      
+     */
+
+    public Dictionary<Vector3Int, bool> GetAdjacentTiles(Vector3Int targetCell)
+    {
+        Dictionary<Vector3Int, bool> adjacentTiles= new Dictionary<Vector3Int, bool>()
+        {
+            {new Vector3Int(targetCell.x-1, targetCell.y-1, tilemapZAxisPosition), allTiles.GetValueOrDefault(new Vector3Int (targetCell.x-1, targetCell.y-1, tilemapZAxisPosition) )},
+            {new Vector3Int(targetCell.x-1, targetCell.y, tilemapZAxisPosition), allTiles.GetValueOrDefault(new Vector3Int (targetCell.x-1, targetCell.y, tilemapZAxisPosition)) },
+            {new Vector3Int(targetCell.x-1, targetCell.y+1, tilemapZAxisPosition), allTiles.GetValueOrDefault(new Vector3Int (targetCell.x-1, targetCell.y+1, tilemapZAxisPosition)) },
+            {new Vector3Int(targetCell.x, targetCell.y-1, tilemapZAxisPosition), allTiles.GetValueOrDefault(new Vector3Int (targetCell.x, targetCell.y-1, tilemapZAxisPosition)) },
+            {new Vector3Int(targetCell.x, targetCell.y+1, tilemapZAxisPosition), allTiles.GetValueOrDefault(new Vector3Int (targetCell.x, targetCell.y+1, tilemapZAxisPosition)) },
+            {new Vector3Int(targetCell.x+1, targetCell.y-1, tilemapZAxisPosition), allTiles.GetValueOrDefault(new Vector3Int (targetCell.x+1, targetCell.y-1, tilemapZAxisPosition)) },
+            {new Vector3Int(targetCell.x+1, targetCell.y, tilemapZAxisPosition), allTiles.GetValueOrDefault(new Vector3Int (targetCell.x+1, targetCell.y, tilemapZAxisPosition)) },
+            {new Vector3Int(targetCell.x+1, targetCell.y+1, tilemapZAxisPosition), allTiles.GetValueOrDefault(new Vector3Int (targetCell.x+1, targetCell.y+1, tilemapZAxisPosition)) },
+        };
+        return adjacentTiles;
+    }
+
+    void ConsiderLivingCells()
+    {
+        foreach(var livingCell in livingCells)
+        {
+            Dictionary<Vector3Int, bool> adjacentTiles = GetAdjacentTiles(livingCell.Key);
+            int livingAdjacentCells = 0;
+            Debug.Log("now measuring " + livingCell);
+            foreach (var cell in adjacentTiles)
+            {
+                if (cell.Value == true)
+                {
+                    livingAdjacentCells += 1;
+                }
+                else if (cell.Value == false && allTiles.ContainsKey(cell.Key))//this is the second time we lookup if the value exists (first is getvalueordefault) OPTIMIZETHIS
+                {
+                    deadCellConsiderationDict[cell.Key] = true;
+                    Debug.Log("dead cell to consider: " + cell.Key);
+                }
+            }
+            if(livingAdjacentCells <2 || livingAdjacentCells > 3)
+            {
+                MarkCellForLifeChange(livingCell.Key, false);
+                Debug.Log("Cell marked for death: " +  livingCell.Key + "living adjacent cells: " + livingAdjacentCells);
+            }
+        }
+    }
+    void ConsiderDeadCells()
+    {
+        foreach (var deadCell in deadCellConsiderationDict)
+        {
+            Dictionary<Vector3Int, bool> adjacentTiles = GetAdjacentTiles(deadCell.Key);
+            int livingAdjacentCells = 0;
+            foreach (var cell in adjacentTiles)
+            {
+                if (cell.Value == true)
+                {
+                    livingAdjacentCells += 1;
+                }
+            }
+            if (livingAdjacentCells == 3)
+            {
+                MarkCellForLifeChange(deadCell.Key, true);
+                Debug.Log("cell marked for birth: " +  deadCell.Key);
+            }
+        }
+        deadCellConsiderationDict.Clear();
+    }
+
+    void MarkCellForLifeChange(Vector3Int targetCell, bool markedToLive)
+    {
+        cellsMarkedForLifeChange.Add(targetCell, markedToLive);
+    }
+
+    void UpdateCellLifeCycle()
+    {
+        foreach(var cell in cellsMarkedForLifeChange)
+        {
+            KillOrBirthCell(cell.Key, cell.Value);
+        }
+        cellsMarkedForLifeChange.Clear();
+    }
+
+    void RunCellLifeCycleLoop()
+    {
+        ConsiderLivingCells();
+        ConsiderDeadCells();
+        UpdateCellLifeCycle();
+    }
 }
+
